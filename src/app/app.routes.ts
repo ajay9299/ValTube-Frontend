44f3d18872dc6e756e2ad7d7c0a79e3115e2c24f
ajay9299 <ajayjangid9299@gmail.com>
2026-03-19 08:19:06 +0530

    login form and api call added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,10 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-    {
-  path: 'auth/login',
-  loadComponent: () =>
-    import('./features/auth/login/login')
-      .then(m => m.Login)
-}
+  {
+    path: 'auth/login',
+    loadComponent: () =>
+      import('./features/auth/login/login')
+        .then(m => m.Login)
+  }
 ];
--- src/app/features/auth/login/login.ts
+++ src/app/features/auth/login/login.ts
@@ -4,6 +4,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MaterialModule } from '../../../shared/material/material-module';
+import { AuthService } from '../../../core/services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -19,30 +20,35 @@ import { MaterialModule } from '../../../shared/material/material-module';
 })
 export class Login {
 
+  constructor(private readonly authService: AuthService) { }
+
   loginForm = new FormGroup({
-    userName: new FormControl('',[
+    username: new FormControl('', [
       Validators.required
     ]),
-    password: new FormControl('',[
+    password: new FormControl('', [
       Validators.required
     ]),
   });
 
   loginError: string = '';
 
-  onSubmit(){
-    console.log(this.loginForm.value);
+  onSubmit() {
     if (this.loginForm.valid) {
       // Simulate authentication logic
-      const { userName, password } = this.loginForm.value;
-      if (userName === 'admin' && password === 'password') {
+      const { username, password } = this.loginForm.value;
+      if (username && password) {
         // Successful login
         this.loginError = '';
-        console.log('Login successful');
-        // Navigate to dashboard or home page
-      } else {
-        // Invalid credentials
-        this.loginError = 'Invalid username or password. Please try again.';
+        this.authService.login({ username, password })
+          .subscribe({
+            next: (res) => {
+              console.log('Login success', res);
+            },
+            error: (err) => {
+              console.log('Login failed', err);
+            }
+          });
       }
     } else {
       this.loginError = 'Please fill in all required fields.';
